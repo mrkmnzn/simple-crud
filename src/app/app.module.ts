bacca998cdf1471737eb7c0ed2d7baf3f342f45b
mrkmnzn <mrkchrstnmnzn@gmail.com>
2023-01-05 20:21:54 +0800

    Added components, interface, guard ..

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './app/user/pages/profile/profile.component';
+import { FormComponent } from './app/user/components/form/form.component';
+import { BookListComponent } from './app/book/pages/book-list/book-list.component';
+import { BookItemComponent } from './app/book/components/book-item/book-item.component';
+import { BlogListComponent } from './app/blog/pages/blog-list/blog-list.component';
+import { BlogItemComponent } from './app/blog/components/blog-item/blog-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    FormComponent,
+    BookListComponent,
+    BookItemComponent,
+    BlogListComponent,
+    BlogItemComponent
   ],
   imports: [
     BrowserModule,
